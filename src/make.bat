d4643ed32c58db1813bd8499202be52fce6af0bf
Daniel Gaskell <daniel.gaskell@yale.edu>
2024-09-21 13:48:45 -0700

    force 32-bit build

--- src/make.bat
+++ src/make.bat
@@ -1,18 +1,18 @@
 @echo off
 
 echo as.c
-gcc -static -static-libgcc -static-libstdc++ as.c -o ..\bin\as.exe
+gcc -m32 -static -static-libgcc -static-libstdc++ as.c -o ..\bin\as.exe
 echo cc.c
-gcc -static -static-libgcc -static-libstdc++ cc.c -o ..\bin\cc.exe
+gcc -m32 -static -static-libgcc -static-libstdc++ cc.c -o ..\bin\cc.exe
 echo cc0.c
-gcc -static -static-libgcc -static-libstdc++ cc0.c -o ..\bin\cc0.exe
+gcc -m32 -static -static-libgcc -static-libstdc++ cc0.c -o ..\bin\cc0.exe
 echo cc1.c
-gcc -static -static-libgcc -static-libstdc++ cc1.c -o ..\bin\cc1.exe
+gcc -m32 -static -static-libgcc -static-libstdc++ cc1.c -o ..\bin\cc1.exe
 echo cc2.c
-gcc -static -static-libgcc -static-libstdc++ cc2.c -o ..\bin\cc2.exe
+gcc -m32 -static -static-libgcc -static-libstdc++ cc2.c -o ..\bin\cc2.exe
 echo ld.c
-gcc -static -static-libgcc -static-libstdc++ ld.c -o ..\bin\ld.exe
+gcc -m32 -static -static-libgcc -static-libstdc++ ld.c -o ..\bin\ld.exe
 echo reloc.c
-gcc -static -static-libgcc -static-libstdc++ reloc.c -o ..\bin\reloc.exe
+gcc -m32 -static -static-libgcc -static-libstdc++ reloc.c -o ..\bin\reloc.exe
 echo copt.c
-gcc -static -static-libgcc -static-libstdc++ copt.c -o ..\bin\copt.exe
+gcc -m32 -static -static-libgcc -static-libstdc++ copt.c -o ..\bin\copt.exe
